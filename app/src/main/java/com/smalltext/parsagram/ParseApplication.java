3b2da17fe77fff530d6882fdf698afd740c98f15
Allie Amerman <aamerman@mit.edu>
2022-01-05 17:56:37 -0500

    Implement posting

--- app/src/main/java/com/smalltext/parsagram/ParseApplication.java
+++ app/src/main/java/com/smalltext/parsagram/ParseApplication.java
@@ -3,11 +3,14 @@ package com.smalltext.parsagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("pWhzciunMmg83bGDwhD3Ja20Fd3aei3tOdbwBg6q")
